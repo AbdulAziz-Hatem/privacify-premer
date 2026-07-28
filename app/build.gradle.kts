13e9b6e03f118a8dc9dc657426aad9cd527f17c6
Robin <robinsrk3@gmail.com>
2026-07-28 07:23:16 +0600

    Bump version to 2.7 (versionCode 27)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.robin.privacify"
         minSdk = 24
         targetSdk = 34
-        versionCode = 26
-        versionName = "2.6"
+        versionCode = 27
+        versionName = "2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
